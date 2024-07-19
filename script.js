f9df43e1855fb6d20f8ecce40c7ae83ef2f2e7f2
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-07-19 19:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
-  // your code here
+  let trimmedStr = s.trim();
+  let spaceIndex = trimmedStr.indexOf(' ');
+  
+  if (spaceIndex == -1) {
+    return trimmedStr;
+  }
+  return trimmedStr.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
